a303a2f458ec979b02b5f76b88bec8cc2f7725a7
Abel Panakkamattom <abelsabu2000@gmail.com>
2026-02-16 20:48:00 +0530

    Add project source files and components

---
+++ src/Containers/App.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import CardList from '../Components/CardList.js';
+import Scroll from '../Components/Scroll.js';
+import Searchbox from '../Components/Searchbox.js';
+/*import {Robot} from './Robot.js';*/
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state ={
+        Robot: [] /*Robot,*/,
+        searchfield:''
+}
+    }
+    
+    componentDidMount() {
+        
+     fetch('https://jsonplaceholder.typicode.com/users')
+     /*fetch('Robot.js')*/
+     .then(response=> response.json())
+     .then(user => this.setState({Robot: user}));
+        /*this.setState({Robot: Robot});*/
+    }
+
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
+        
+        
+    } 
+    render() {
+    const {Robot, searchfield} = this.state;
+    const filteredRobot = Robot.filter(Robot =>{
+      return Robot.name.toLowerCase().includes(searchfield.toLowerCase());
+        });
+
+    return !Robot.length ?
+    
+         <h1>Loading</h1> :
+     (
+    <div className="tc">
+     <h1 className='f1'> Robo Buddies </h1>
+     <Searchbox searchChange={this.onSearchChange}/>
+     <Scroll>
+    <CardList Robot={filteredRobot} />
+    </Scroll>
+    </div>
+    )
+}
+    }
+export default App;
---
+++ src/Robot.js
@@ -0,0 +1,121 @@
+export const Robot =[
+    {
+        id: 1,
+        name: 'Anish John Sabu',
+        username: 'Anish',
+        email: 'ReachAnish1512@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Siril Sarah Sabu',
+        username: 'Siril',
+        email: 'SirilSabu03@gmail.com'
+    },
+    {
+        id: 3,
+        name: 'Ashish James Samuel',
+        username: 'Ashish',
+        email: 'Samashish13@gmail.com'
+    },
+    {
+        id: 4,
+        name: 'Abel Sabu Panakkamattom',
+        username:'Abel',
+        email:'AbelSabu1514@gmail.com'
+    },
+    {
+        id: 5,
+        name: 'Subil Mariam Sabu',
+        username:'Subil',
+        email: 'SubilChan13@gmail.com'
+    },
+    {
+        id: 6,
+        name: 'Shravan Rajesh Gupta',
+        username: 'Shravan',
+        email: 'GuptaShravan794@gmail.com'
+   },
+   {
+        id: 7,
+        name: 'Atharva Santosh Morkar',
+        username: 'Atharva',
+        email: 'Morkar.Atharva302@gmail.com'
+   },
+   {
+        id: 8,
+        name: 'Vedant Umesh Dulam',
+        username: 'Vedant',
+        email: 'VedantDulam@gmail.com'
+   },
+   {
+        id: 9,
+        name: 'Yash Sachin Divate',
+        username: 'Yash',
+        email: 'YashDivate6822@gmail.com'
+   },
+   {
+        id: 10,
+        name: 'Aaron Vinod Wadkar',
+        username: 'Aaron',
+        email: 'aaronwadkar@gmail.com'
+   },
+   {
+        id: 11,
+        name: 'Zer Vinod Wadkar',
+        username:'Zer',
+        email: 'zerwadkar@gmail.com'
+   },
+   {
+        id: 12,
+        name: 'Sabu Chacko Oommen',
+        username: 'Sabu',
+        email: 'SabuOommen66@gmail.com'
+   },
+   {
+        id: 13,
+        name: 'Leonardo Da Vinci',
+        username: 'Da Vinci',
+        email: 'LeonardoDaVinci@gmail.com'
+   },
+   {
+            id: 14,
+               name: 'Albert Einstein',
+               username: 'Einstein',
+               email: 'AlbertEinstein@gmail.com'
+   },
+   {
+            id: 15,
+               name: 'Isaac Newton',
+               username: 'Newton',
+               email: 'IsaacNewton@gmail.com'
+   },
+   {
+               id: 16,
+               name: 'Nikola Tesla',
+               username: 'Tesla',
+               email: 'NikolaTesla@gmail.com'
+   },
+   {
+               id: 17,
+               name: 'Galileo Galilei',
+               username: 'Galilei',
+               email: 'GalileoGalilei@gmail.com'
+   },
+   {
+      id:18,
+      name: 'Marie Curie',
+      username: 'Curie',
+      email: 'MarieCurie@gmail.com'
+   },
+     {         id:19,
+               name: 'Charles Darwin',
+               username: 'Darwin',
+               email: 'CharlesDarwin@gmail.com'
+     },
+     {
+               id:20,
+               name: 'Stephen Hawking',
+               username: 'Hawking',
+               email: 'StephenHawking@gmail.com'
+   }
+]
